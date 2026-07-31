e3c57d2369768fe3218ba9a40ff77dfa778e47b4
Andy <andyherbert1875@gmail.com>
2026-07-31 17:51:28 +0100

    Final deploy changes

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const app = express()
 
@@ -9,7 +8,6 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(express.json())
 app.use(express.static('dist'))
-app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
